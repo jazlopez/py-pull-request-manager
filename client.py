3ef30d71588a39a8f2362ad9e19625e22e73a3e6
Jaziel Lopez <juan.jaziel@gmail.com>
2019-08-18 11:40:29 -0700

    client.py

--- client.py
+++ client.py
@@ -9,6 +9,10 @@ def authorize(user_token=None):
     :param user_token:
     :return:
     """
+
+    if not user_token:
+        raise ValueError("you must provide user token")
+
     return Github(login_or_token=user_token)
 
 
@@ -19,6 +23,12 @@ def get_open_pull_requests(repo=None, base=None):
     :param base:
     :return:
     """
+
+    if not repo:
+        raise ValueError("you must provide repo")
+
+    if not base:
+        raise ValueError("you must provide base branch")
 
     return repo.get_pulls(state='open', sort='created', base=base)
 
