ec4aebb4cb90d4a6ec6baa78b6d2ff61706e29c6
Jaziel Lopez <juan.jaziel@gmail.com>
2019-08-18 00:48:52 -0700

    settings.py

---
+++ settings.py
@@ -0,0 +1,9 @@
+import os
+
+# settings module
+
+GITHUB_USER_TOKEN = os.getenv("GITHUB_USER_TOKEN")
+GITHUB_REPOSITORY = os.getenv("GITHUB_REPOSITORY_PULL_REQUEST")
+GITHUB_BASE_PULL_REQUEST = os.getenv("GITHUB_BASE_PULL_REQUEST")
+
+__author__ = "Jaziel Lopez at jlopez.mx"
