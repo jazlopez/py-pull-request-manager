3bab31f7546d93f8540b4cc38953f8f9dffaea66
Jaziel Lopez <juan.jaziel@gmail.com>
2019-08-25 00:39:26 -0700

    settings(.env.dist)

--- settings.py
+++ settings.py
@@ -2,6 +2,16 @@ import os
 
 # settings module
 
+try:
+    from dotenv import load_dotenv
+
+    if not os.path.exists(os.path.abspath(os.path.join(os.path.dirname(__file__), '.env'))):
+        raise FileNotFoundError("expected .env file could not be found")
+
+    load_dotenv(verbose=True, override=True)
+except FileNotFoundError or ImportError:
+    pass
+
 GITHUB_USER_TOKEN = os.getenv("GITHUB_USER_TOKEN")
 GITHUB_REPOSITORY = os.getenv("GITHUB_REPOSITORY_PULL_REQUEST")
 GITHUB_BASE_PULL_REQUEST = os.getenv("GITHUB_BASE_PULL_REQUEST")
